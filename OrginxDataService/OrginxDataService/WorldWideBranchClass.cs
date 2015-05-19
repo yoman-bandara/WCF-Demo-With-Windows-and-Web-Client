14b96b85b8c37ae496b58f4478c4ed5419741041
yoman-bandara <yomanbanda@gmail.com>
2015-05-19 23:49:05 +0530

    Added World Wide Branch Class and Service

---
+++ OrginxDataService/OrginxDataService/WorldWideBranchClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrginxDataService
+{
+   public class WorldWideBranchClass
+    {
+        private string officename;
+        private string officecountry;
+        private string officeadmin;
+        private string officedesc;
+        public string Officename
+        {
+            get { return officename; }
+            set { officename = value; }
+        }
+      
+       public string Officecountry
+       {
+           get { return officecountry; }
+           set { officecountry = value; }
+       }
+      
+
+       public string Officeadmin
+       {
+           get { return officeadmin; }
+           set { officeadmin = value; }
+       }
+      
+
+       public string Officedesc
+       {
+           get { return officedesc; }
+           set { officedesc = value; }
+       }
+    }
+}
